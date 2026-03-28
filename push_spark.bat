086510a7df207bbff011d2e869ea3d16be5c3e77
Mimo <mimo@example.com>
2026-03-28 19:58:46 +0100

    Mimo Spark First Commit

--- push_spark.bat
+++ push_spark.bat
@@ -1,14 +1,15 @@
 @echo off
-echo --- MIMO SPARK - PUSH FINAL ---
+echo --- MIMO SPARK - PUSH FINAL (ULTIME) ---
 git config --global user.email "mimo@example.com"
 git config --global user.name "Mimo"
 git init
-git add .
-git commit -m "Full Mimo Spark App - Mimo Initial Push"
+git branch -M main
+git remote remove origin
 git remote add origin https://github.com/djenadimohamedamine-code/spark.git
+git add .
+git commit -m "Mimo Spark First Commit"
 git push -u origin main --force
 echo.
-echo --- TERMINE ! ---
-echo Si tu vois des messages d'erreur, prends une photo car la fenetre va rester ouverte.
-echo Sinon, appuie sur une touche pour quitter.
+echo --- SUCCESS ! ---
+echo Verifie GitHub maintenant.
 pause
