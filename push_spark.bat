e8631f0ffef5c598861dcaace6baa2a6607672e6
Mimo <mimo@example.com>
2026-04-02 16:02:55 +0200

    Mimo Spark: UI Fix, Menu Lateral and Dashboard Optimization

--- push_spark.bat
+++ push_spark.bat
@@ -1,5 +1,5 @@
 @echo off
-echo --- PUSH & BUILD MIMO_SPARK ---
+echo --- PUSH ^& BUILD MIMO_SPARK ---
 git add .
 git commit -m "Mimo Spark: UI Fix, Menu Lateral and Dashboard Optimization"
 git push
